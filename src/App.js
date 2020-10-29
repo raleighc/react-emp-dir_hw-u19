0acf7db6bb5d151e392b65781ff7c3d2e02ba43e
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-29 02:09:29 -0400

    Created a header layout and rendered it onto the page via App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import Header from "./components/layout/Header";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>Hello World!</h1>
-      </div>
+      <>
+        <Header />
+        <div className="container">
+          <div className="row"></div>
+        </div>
+      </>
     );
   }
 }
