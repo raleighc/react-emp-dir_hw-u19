877ecf09fdf373c238daa096e500fe124292d842
Raleigh Chesney <whileiwasthinking@yahoo.com>
2020-10-29 02:21:46 -0400

    added content to and styled header.

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,10 +2,17 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <header>
+        <header className="text-center text-white" style={headerStyle}>
             <h1>Employee Directory</h1>
+            <p>Click on carrots to filter by heading or use the search box to narrow your results.</p>
         </header>
     );
 };
+
+const headerStyle = {
+    background: '#204064',
+    padding: '20px',
+    borderBottom: "4px solid red"
+}
 
 export default Header;
